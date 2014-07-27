b37c41d437e512e27f075858f6e0512ae9d8cdcc
MARCUS HSU <mh3346@columbia.edu>
2014-07-27 15:30:55 -0400

    Finished twitterAPI.py

--- twitterAPI.py
+++ twitterAPI.py
@@ -10,7 +10,8 @@ csecret = "BHPGUHFKk66cRL40MR4rXec26DsrKtXa36iii92wyBZcrWZ9tb"
 atoken = "1914184802-niSHlUKUWZaVO3h2seLSxIcrDnFy4zu5tJDNXGc"
 asecret = "axdMrkkUavyI7UhUpiRdtLlZ1v2JqYPYPtubfj4mmuer9"
 
-#SAVE_FILE_NAME = "tweet04.txt"
+save_file_name = "default"
+SAVE_PATH = "data/"
 
 class listener(StreamListener):
 	# keep looping on this function
@@ -27,7 +28,7 @@ class listener(StreamListener):
 
 		# declate as global, which althorize the function can change 
 		# the value locally
-		global SAVE_FILE_NAME
+		global save_file_name
 
 		try:
 			if type(data) is str:
@@ -36,17 +37,18 @@ class listener(StreamListener):
 				created_at = data.split('"created_at":"')[1].split('","id":')[0]
 				#print tweet
 				#print created_at
+			relativePath = os.path.join(SAVE_PATH, save_file_name+".txt")   
 			# "a+" : append  
-			with open(SAVE_FILE_NAME, "a+") as output_file:
-				file_size = os.stat(SAVE_FILE_NAME)
-				#output_file.write("here")
-				print file_size.st_size
+			with open(relativePath, "a+") as output_file:
+				file_size = os.stat(relativePath).st_size
+				print "current size : " + str(file_size)
 				# set file size < 1000B(1KB)
-				if file_size.st_size < 1000:
+				if file_size < 1000:
 					output_file.write("Tweet : "+tweet + "\n")
 					output_file.write("Created at : " + created_at+"\n")
 					output_file.write("\n")
 				else:
+					print "exceed the file size limit !!"
 					# need to disconncect with tweepy, still figuring out..
 					pass	
 		except BaseException, e:
@@ -57,19 +59,28 @@ class listener(StreamListener):
 		print "error", status
 
 def main():
-	global SAVE_FILE_NAME
-	SAVE_FILE_NAME = raw_input("Please enter a file name to save as : ")
-	if os.path.exists(SAVE_FILE_NAME):
-		os.remove(SAVE_FILE_NAME)
+	global save_file_name
+	global SAVE_PATH
+
+	print "This tool can can help you retreive Twitter's data through Twitter API"
 	print "starting.."
+
+	save_file_name = raw_input("Please enter a file name to save as : ")
+
+	relativePath = os.path.join(SAVE_PATH, save_file_name+".txt")   
+	if os.path.exists(relativePath):
+		os.remove(relativePath)
+
 	# keyword for searching  
-	keyword = raw_input("What do you wanna search this time?\n")
+	keyword = raw_input("What do you wanna search this time?  ")
+
 	auth = OAuthHandler(ckey, csecret)
 	auth.set_access_token(atoken, asecret)
 	twitterStream = Stream(auth, listener())
 	twitterStream.filter(track=[keyword])
 	# the following line actually can't work
 	twitterStream.disconnect()
+
 	print "This is the end of the program"
 
 if __name__ == "__main__":
